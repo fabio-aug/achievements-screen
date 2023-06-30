64b69c7828f6187d7fccc85f196263f03fb49b27
Fábio Augusto <fabio.augusto1911@gmail.com>
2023-06-30 09:35:16 -0300

    Update AchievementsViewModel.java

--- app/src/main/java/com/ifmg/trofeujoinha/AchievementsViewModel.java
+++ app/src/main/java/com/ifmg/trofeujoinha/AchievementsViewModel.java
@@ -1,5 +1,6 @@
 package com.ifmg.trofeujoinha;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -8,10 +9,24 @@ public class AchievementsViewModel extends ViewModel {
     public MutableLiveData<String> surname = new MutableLiveData<>();
     public MutableLiveData<Integer> like = new MutableLiveData<>();
 
+    public MutableLiveData<Integer> image = new MutableLiveData<>();
+
     public AchievementsViewModel() {
         this.name.setValue("Name");
         this.surname.setValue("Surname");
         this.like.setValue(0);
+        this.image.setValue(R.drawable.trophy);
+    }
+
+    public void setImage() {
+        int aux = this.like.getValue();
+        if (aux <= 10) {
+            image.setValue(R.drawable.trophy);
+        } else if (aux <= 20) {
+            image.setValue(R.drawable.trophy2);
+        } else {
+            image.setValue(R.drawable.trophy3);
+        }
     }
 
     public MutableLiveData<String> getName() {
@@ -33,5 +48,10 @@ public class AchievementsViewModel extends ViewModel {
     public void setLike() {
         int aux = this.like.getValue();
         this.like.setValue(aux + 1);
+        setImage();
+    }
+
+    public LiveData<Integer> getImage() {
+        return image;
     }
 }
